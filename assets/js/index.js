46843dd109b49893d8860e29abcce0a9004cabde
Volkov <mark.volkovgit@gmail.com>
2021-09-25 16:28:41 +0300

    create Timer

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const timeContainer = document.querySelector(".timerContainer");
+const timerControlsContainer = document.querySelector(".timerControlsContainer");
+
+const timer = new Timer(timeContainer, timerControlsContainer);
+
+
+
+
